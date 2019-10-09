d85f736f5bed5f9a68a8cadeddf81a96e4b9fbee
Luisfer25a <40745719+Luisfer25a@users.noreply.github.com>
2019-10-09 13:02:40 -0600

    Commit2

--- build/controllers/songsController.js
+++ build/controllers/songsController.js
@@ -1,22 +1,77 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+let Canciones = [];
 class SongsController {
     list(req, res) {
-        res.json({ text: 'enlistando canciones' });
+        if (Canciones.length > 0) {
+            res.json({ Canciones });
+            res.json({ Response: '200' });
+        }
+        else {
+            res.json({ Response: '404 Not found' });
+        }
     }
     listone(req, res) {
-        res.json({ text: 'enlistando cancion' + req.params.id });
+        let Cancion = "";
+        for (let i = 0; i < Canciones.length; i++) {
+            if (Canciones[i].nombre == req.params.id) {
+                Cancion += JSON.stringify(Canciones[i]) + ',';
+            }
+        }
+        if (Cancion == "") {
+            res.json({ Response: '404 Not found' });
+        }
+        else {
+            res.json({ Cancion });
+            res.json({ Response: '200' });
+        }
     }
     create(req, res) {
         console.log(req.body);
-        localStorage.setItem(req.body.nombre, req.body);
-        res.json({ text: 'creada cancion' });
+        Canciones.push((req.body));
+        console.log(Canciones);
+        res.json({ Response: '201' });
     }
     delete(req, res) {
-        res.json({ text: 'eliminando cancion' + req.params.id });
+        let Canciones2 = [];
+        let count = 0;
+        for (let i = 0; i < Canciones.length; i++) {
+            if (Canciones[i].nombre == req.params.id) {
+                count++;
+            }
+            else {
+                Canciones2.push(Canciones[i]);
+            }
+        }
+        if (count > 0) {
+            Canciones = Canciones2;
+            res.json({ Response: '204' });
+        }
+        else {
+            res.json({ Response: '404 Not Found' });
+        }
     }
     update(req, res) {
-        res.json({ text: 'actualizando cancion' + req.params.id });
+        let Canciones2 = [];
+        let count = 0;
+        for (let i = 0; i < Canciones.length; i++) {
+            if (Canciones[i].nombre == req.params.id) {
+                count++;
+            }
+            else {
+                Canciones2.push(Canciones[i]);
+            }
+        }
+        if (count > 0) {
+            Canciones = Canciones2;
+            console.log(req.body);
+            Canciones.push((req.body));
+            console.log(Canciones);
+            res.json({ Response: '204' });
+        }
+        else {
+            res.json({ Response: '404 Not Found' });
+        }
     }
 }
 const songsController = new SongsController();
--- src/controllers/songsController.ts
+++ src/controllers/songsController.ts
@@ -1,23 +1,83 @@
 import { Request , Response } from 'express';
 
+let Canciones: any[] = []
 class SongsController{
-
+    
     public list (req: Request,res: Response) {
-        res.json({text: 'enlistando canciones'});
+       if (Canciones.length>0){ 
+        res.json({Canciones});
+        res.json({Response: '200'});
+       }
+       else{
+        res.json({Response: '404 Not found'});
+       }
+
     }
     public listone (req: Request,res: Response) {
-        res.json({text: 'enlistando cancion' + req.params.id});
+        let Cancion:String = "";
+        for (let i = 0;i<Canciones.length;i++){
+            if (Canciones[i].nombre == req.params.id){
+                Cancion += JSON.stringify(Canciones[i]) + ',';
+                
+            }
+        }
+        if (Cancion == ""){
+            res.json({Response: '404 Not found'});
+        }
+        else{
+            res.json({Cancion});
+            res.json({Response: '200'});
+        }
+        
     }
     public create (req:Request, res:Response){
         console.log(req.body);
-        localStorage.setItem(req.body.nombre, req.body);
-        res.json({text: 'creada cancion'});
+        Canciones.push((req.body));
+        console.log (Canciones);
+        res.json({Response: '201'});
     }
     public delete (req:Request, res:Response){
-        res.json({text: 'eliminando cancion'+ req.params.id});
+        let Canciones2: any[] = []
+        let count = 0;
+        for (let i = 0;i<Canciones.length;i++){
+            if (Canciones[i].nombre == req.params.id){
+                count++;
+            }
+            else{
+                Canciones2.push(Canciones[i]);
+            }
+        }
+        if (count >0){
+            Canciones = Canciones2;
+            res.json({Response: '204'});
+        }
+        else{
+            res.json({Response: '404 Not Found'});
+        }
+
     }
     public update (req:Request, res:Response){
-        res.json({text: 'actualizando cancion'+ req.params.id});
+        let Canciones2: any[] = []
+        let count = 0;
+        for (let i = 0;i<Canciones.length;i++){
+            if (Canciones[i].nombre == req.params.id){
+                count++;
+            }
+            else{
+                Canciones2.push(Canciones[i]);
+            }
+        }
+        if (count >0){
+            Canciones = Canciones2;
+            console.log(req.body);
+            Canciones.push((req.body));
+            console.log (Canciones);
+            res.json({Response: '204'});
+        }
+        else{
+            res.json({Response: '404 Not Found'});
+        }
+        
     }
 
 }
