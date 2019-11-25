088b2e5950f99d89aed9dea9e40975455a6c0e2d
Luisfer25a <40745719+Luisfer25a@users.noreply.github.com>
2019-11-25 16:59:45 -0600

    Changes with docker

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/CrudDB', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(console.log('Connected to MongoDB')).catch(error=> handleError(error));
+mongoose.connect('mongodb://mongo:27017/CrudDB', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).then(console.log('Connected to MongoDB')).catch(error=> handleError(error));
 
 module.exports = mongoose;
